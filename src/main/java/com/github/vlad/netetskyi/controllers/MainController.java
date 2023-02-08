9bd009db2b5f4fb64ac955d9cf2c9e80c08119b6
Vladyslav Netetskyi <thermwaik@gmail.com>
2023-02-08 18:27:07 +0200

    added skeleton web-application

---
+++ src/main/java/com/github/vlad/netetskyi/controllers/MainController.java
@@ -0,0 +1,14 @@
+package com.github.vlad.netetskyi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String greeting( Model model) {
+        model.addAttribute("title", "main page");
+        return "index";
+    }
+}
